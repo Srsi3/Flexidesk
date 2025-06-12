cb3ede2b1e979f11495f8a934d55eb75a426fdf4
srsi3 <ssaldanha03@gmail.com>
2025-06-11 23:39:28 -0400

    pch

---
+++ src/Widget_host/pch.h
@@ -0,0 +1,9 @@
+#pragma once
+
+// C++/WinRT headers
+#include <winrt/Windows.Foundation.h>
+#include <winrt/Microsoft.UI.Xaml.h>
+#include <winrt/Microsoft.UI.Xaml.Controls.h>
+#include <winrt/Microsoft.UI.Windowing.h>
+#include <winrt/WinRT.Interop.h>
+#include <winrt/Windows.UI.h>
